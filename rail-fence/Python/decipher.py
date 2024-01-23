4f1841ab1cdedc328e39ce5ae6ea76a3e4304c47
Umang <umangamrania16@outlook.com>
2024-01-23 20:24:15 +0530

    Rail-fence: decipher: element count working

--- rail-fence/Python/decipher.py
+++ rail-fence/Python/decipher.py
@@ -2,7 +2,7 @@ from math import floor
 
 def decipher(cipher_text: str, depth: int):
     ct_len = len(cipher_text)
-    tick_size = 2 * depth - 2
+    tick_size = (2 * depth) - 2
     p = floor( ct_len / tick_size )
     r = ct_len % tick_size
 
@@ -13,16 +13,30 @@ def decipher(cipher_text: str, depth: int):
     matrix = [[] for _ in range(depth)] # preparing empty matrix
     element_count = [0 for _ in range(depth)] # stores no of elements in each row
     
+    element_count[0] = p
+    element_count[-1] = p
+
+    if r > depth:
+        element_count[0] += 1
+        element_count[-1] += 1
+
+    # processing middle rows
+    i = 1
+    while i < len(element_count) - 1:
+        element_count[i] = p * 2
+        i += 1
+
+    if r > depth:
+        i = depth - 2
+        while i > r - depth:
+            element_count[i] += 1
+            i -= 1
+    else:
+        i = 0
+        while i < r:
+            element_count[i] += 1
+            i += 1
+
     print(element_count)
 
-    element_count[0] = p + 1 if r > depth else 0
-    element_count[-1] = p + 1 if r > depth else 0
-
-    i = depth - 2
-    while i > r - depth:
-        element_count[i] += 1
-        i -= 1
-
-    print(element_count)
-
-decipher("rojtaktuaajgr", 3)
+decipher("rjiauadajgrnaktaiot", 5)
